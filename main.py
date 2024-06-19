903d5fddb09ea20f8e8777f240ba2f6b1a6afc53
alejandro.baldres@gmail.com <alejandro.baldres@gmail.com>
2024-06-19 18:45:34 -0300

    ejecucion de subproceso de flask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import repository.repositoryDB as db
+import subprocess
 
-basedatos = db.baseDeDatos("prueba","XXXX")
+basedatos = db.baseDeDatos("###","XXX")
 basedatos.creoBaseDeDatos("comiendohastaloscodos")
 basedatos.creoTablas("comiendohastaloscodos")
 basedatos.cierroConeccion()
+subprocess.run(["flask --app ./controller/controladorFlask.py run"],shell=True,)
